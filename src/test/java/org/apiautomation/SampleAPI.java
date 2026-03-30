1a1cfef3063f3d6c4058e7eacb93cb94331e8f9c
srin-omr <srinivasanomrbranch@gmail.com>
2026-03-30 16:18:44 +0530

    March 30

--- src/test/java/org/apiautomation/SampleAPI.java
+++ src/test/java/org/apiautomation/SampleAPI.java
@@ -1,6 +1,8 @@
 package org.apiautomation;
 
 import org.json.pojo.CreateFlight_Output_pojo;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
@@ -8,7 +10,10 @@ import io.restassured.specification.RequestSpecification;
 
 public class SampleAPI {
 
-	public static void main(String[] args) {
+	int id;
+
+	@Test
+	public void createFlight() {
 
 		RequestSpecification req;
 
@@ -35,11 +40,42 @@ public class SampleAPI {
 
 		String message = as.getMessage();
 
-		System.out.println(message);
+		Assert.assertEquals(message, "Flight Created Successfully", "Verify the message");
 
 		String flightName = as.getData().getFlightName();
 
-		System.out.println(flightName);
+		Assert.assertEquals(flightName, "AirIndia", "Verify the flight name");
+
+		id = as.getData().getId();
+
+		System.out.println("Flight id " + id);
+
+	}
+
+	@Test
+	public void updateFlight() {
+
+		System.out.println("Flight id " + id);
+		RequestSpecification req;
+
+		req = RestAssured.given().header("Content-Type", "application/json");
+
+		req = req.body("{\r\n" + "    \"flightName\": \"AirIndia\",\r\n" + "    \"Country\": \"America\",\r\n"
+				+ "    \"Destinations\": 67,\r\n"
+				+ "    \"URL\": \"https:\\/\\/en.wikipedia.org\\/wiki\\/Air_India\"\r\n" + "}");
+		Response res = req.put("https://www.omrbranch.com/api/flight/" + id);
+
+		int statusCode = res.getStatusCode();
+
+		System.out.println(statusCode);
+
+		String asString = res.asString();
+
+		System.out.println(asString);
+
+		String asPrettyString = res.asPrettyString();
+
+		System.out.println(asPrettyString);
 	}
 
 }
